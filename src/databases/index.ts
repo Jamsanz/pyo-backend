befe12b5933b6756cd9ec25659b2038d6bde25c5
Muhammad <jamiludeensani@gmail.com>
2022-08-26 10:24:50 +0100

    Heroku

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -4,7 +4,7 @@ const localMongoUri = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
 const mongoUri = `mongodb+srv://admin-deeni:muha234mmad@cluster0.n2lhm.mongodb.net/pyo-institute`;
 
 export const dbConnection = {
-  url: process.env.NODE_ENV !== 'development' ? localMongoUri : mongoUri,
+  url: process.env.NODE_ENV === 'development' ? localMongoUri : mongoUri,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
